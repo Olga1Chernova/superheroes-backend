ed79fab99f79383c6706dba913ffefdba1fd376d
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-30 22:56:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+const app = require('./app')
 
-const app = require('./app');
+require("dotenv").config();
 
-const { DB_HOST, PORT = 3000 } = process.env;
-  
+const { DB_HOST, PORT } = process.env;
+
 mongoose.connect(DB_HOST)
-  .then(() => app.listen(PORT))
-  .catch(error => console.log(error.message));
-
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
